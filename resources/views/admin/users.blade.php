960f5da80fdcff2b873a308a82896748394599de
TheMasz <one.oi910@gmail.com>
2024-01-24 19:07:17 +0700

    add favicon

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -29,6 +29,14 @@
             border-bottom: 2px solid #009878;
         }
     </style>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
     <title>Admin User</title>
 </head>
 
@@ -150,8 +158,8 @@
                                             </button></a>
 
                                         <a href='#delete{{ $item->member_id }}' data-toggle='modal'><button
-                                                class="btn btn-danger btn-sm" title="ลบข้อมูล" type="submit"><span><i
-                                                        class="fa-solid fa-trash"></i></span>
+                                                class="btn btn-danger btn-sm" title="ลบข้อมูล"
+                                                type="submit"><span><i class="fa-solid fa-trash"></i></span>
                                             </button></a>
                                 </tr>
                             @endforeach
